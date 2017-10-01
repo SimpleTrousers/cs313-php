ed99aad563089227955cfe2b3779fa1f2036e8dc
SimpleTrousers <simpletrousers2@gmail.com>
2017-09-30 21:45:59 -0600

    Function!

--- web/cart.php
+++ web/cart.php
@@ -8,28 +8,27 @@
     <link rel="stylesheet" type="text/css" href="shopping.css">
 </head>
 <body>
+    <?php
+    $baboon = $_POST["baboonChk"];
+    $howler = $_POST["howlerChk"];
+    $capuchin = $_POST["capuchinChk"];
+    $marmoset = $_POST["marmosetChk"];
+
+    function baboon() {
+        echo $baboon;
+        if ($baboon == "baboon") {
+            echo "POOOP";
+        }
+    }
+    ?>
+
     <h1>Your Cart!</h1>
     <form action="checkout.php" method="post">
         <input class="submit" type="submit" value="Checkout!">
     </form>
     <?php echo $_POST["marmosetChk"];
     echo $_POST["howlerChk"];
+    baboon();
     echo $_POST["capuchinChk"]; ?>
 </body>
 </html>
-
-<?php
-$baboon = $_POST["baboonChk"];
-$howler = $_POST["howlerChk"];
-$capuchin = $_POST["capuchinChk"];
-$marmoset = $_POST["marmosetChk"];
-
-function baboon() {
-    echo $baboon;
-    if ($baboon == "baboon") {
-        echo "POOOP";
-    }
-}
-
-baboon();
-?>
