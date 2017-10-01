b45434fb01ae390d4e8ded47d5ed198fe3f59ca6
SimpleTrousers <simpletrousers2@gmail.com>
2017-09-30 20:27:51 -0600

    Table better

--- web/browse.php
+++ web/browse.php
@@ -11,15 +11,35 @@
     <form action="POST">
         <table>
             <tr>
+                <th>
+                    Monkey Species
+                </th>
+                <th>
+                    Description
+                </th>
+                <th>
+                    Price
+                </th>
+                <th>
+                    Purchase?
+                </th>
+            </tr>
+            <tr>
                 <td>
                     <img src="baboon.jpg" />
                 </td>
+            </tr>
+            <tr>
                 <td>
                     <img src="capuchin.jpg" />
                 </td>
+            </tr>
+            <tr>
                 <td>
                     <img src="howler.jpg" />
                 </td>
+            </tr>
+            <tr>
                 <td>
                     <img src="marmoset.jpg" />
                 </td>
