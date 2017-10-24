d8c72bbeb2e0021b3bd1761e8104e76140c37177
SimpleTrousers <simpletrousers2@gmail.com>
2017-10-23 20:21:51 -0600

    Moved some things around

---
+++ 04Prove.sql
@@ -0,0 +1,25 @@
+create table public.user
+(
+	id serial not null primary key,
+	username varchar(100) not null unique,
+	password varchar(100) not null,
+	first_name varchar(100) not null,
+	last_name varchar(100) not null,
+	admin boolean not null
+);
+
+create table public.history
+(
+	id serial not null primary key,
+	nurse_id int not null references public.user(id),
+	appointment_id int not null references public.appointment(id),
+	notes varchar(500) not null
+);
+
+create table public.appointment
+(
+	id serial not null primary key,
+	date date not null,
+	doctor_id int not null references public.user(id),
+	patient_id int not null references public.user(id)
+);
