040bc025db64828a7aff37cb7b5ab1922db36248
SimpleTrousers <simpletrousers2@gmail.com>
2017-10-23 20:40:19 -0600

    Added session files

---
+++ web/Prove04/session1.php
@@ -0,0 +1,21 @@
+<?php
+// Start the session
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+// Set session variables
+$_SESSION["favcolor"] = "green";
+$_SESSION["favanimal"] = "cat";
+// Echo session variables that were set on previous page
+echo "Favorite color is " . $_SESSION["favcolor"] . ".<br>";
+echo "Favorite animal is " . $_SESSION["favanimal"] . ".";
+?>
+
+<a href="https://cryptic-refuge-67781.herokuapp.com/Prove04/session2.php">Next Session</a>
+
+</body>
+</html>
