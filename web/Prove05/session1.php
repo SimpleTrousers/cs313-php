6be9a6a28ffd52c36849060f7ce77fb1e043334b
SimpleTrousers <simpletrousers2@gmail.com>
2017-10-25 16:07:22 -0600

    Hopefully can connect to database now.

--- web/Prove05/session1.php
+++ web/Prove05/session1.php
@@ -13,6 +13,42 @@ $_SESSION["favanimal"] = "cat";
 // Echo session variables that were set on previous page
 echo "Favorite color is " . $_SESSION["favcolor"] . ".<br>";
 echo "Favorite animal is " . $_SESSION["favanimal"] . ".";
+
+// default Heroku Postgres configuration URL
+$dbUrl = getenv('DATABASE_URL');
+
+$dbopts = parse_url($dbUrl);
+
+print "<p>$dbUrl</p>\n\n";
+
+$dbHost = $dbopts["host"];
+$dbPort = $dbopts["port"];
+$dbUser = $dbopts["user"];
+$dbPassword = $dbopts["pass"];
+$dbName = ltrim($dbopts["path"],'/');
+
+print "<p>pgsql:host=$dbHost;port=$dbPort;dbname=$dbName</p>\n\n";
+
+try
+{
+	// Create the PDO connection
+	$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+	// this line makes PDO give us an exception when there are problems, and can be very helpful in debugging!
+	$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+}
+catch (PDOException $ex)
+{
+	// If this were in production, you would not want to echo
+	// the details of the exception.
+	echo "Error connecting to DB. Details: $ex";
+	die();
+}
+
+foreach ($db->query('SELECT now()') as $row)
+{
+ print "<p>$row[0]</p>\n\n";
+}
+
 ?>
 
 <a href="https://cryptic-refuge-67781.herokuapp.com/Prove04/session2.php">Next Session</a>
