30871d4a373c7b9f5e4c8ee51fcb0750a7c5b36e
SimpleTrousers <simpletrousers2@gmail.com>
2017-10-25 18:37:16 -0600

    Added some stuff.

--- web/Prove05/login.php
+++ web/Prove05/login.php
@@ -1,9 +1,18 @@
+<?php
+// Start the session
+session_start();
+?>
+
 <!DOCTYPE <html>
 <html>
 <body>
     <h1>Please Log In!</h1>
 
     <form action="redirect.php" method="post">
+        Username:
+        <input type="text" name="uname"><br>
+        Password:
+        <input type="text" name="pass"><br>
         <input type="submit">
     </form>
 </body>
