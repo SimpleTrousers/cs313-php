c49fef470d016d89d36a15f4f4a7d13a5609b6cf
SimpleTrousers <simpletrousers2@gmail.com>
2017-10-27 13:21:56 -0600

    More if statement tests.

--- web/Prove05/redirect.php
+++ web/Prove05/redirect.php
@@ -1,35 +1,4 @@
 <?php
-session_start();
-
-// default Heroku Postgres configuration URL
-$dbUrl = getenv('DATABASE_URL');
-
-$dbopts = parse_url($dbUrl);
-
-$dbHost = $dbopts["host"];
-$dbPort = $dbopts["port"];
-$dbUser = $dbopts["user"];
-$dbPassword = $dbopts["pass"];
-$dbName = ltrim($dbopts["path"],'/');
-
-try
-{
-	// Create the PDO connection
-	$_SESSION['db'] = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-	// this line makes PDO give us an exception when there are problems, and can be very helpful in debugging!
-    $_SESSION['db']->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-}
-catch (PDOException $ex)
-{
-	// If this were in production, you would not want to echo
-	// the details of the exception.
-	echo "Error connecting to DB. Details: $ex";
-	die();
-}
-
-function myTest() {
-    $GLOBALS['y'] = $GLOBALS['x'] + $GLOBALS['y'];
-} 
 
 function appointment() {
     $stmt = $_SESSION['db']->prepare('SELECT * FROM public.user where username=:uname and password=:pword');
@@ -57,6 +26,33 @@ function appointment() {
 }
 
 if ($_POST['pageFrom'] == 'loginPage') {
+    session_start();
+    
+    // default Heroku Postgres configuration URL
+    $dbUrl = getenv('DATABASE_URL');
+    
+    $dbopts = parse_url($dbUrl);
+    
+    $dbHost = $dbopts["host"];
+    $dbPort = $dbopts["port"];
+    $dbUser = $dbopts["user"];
+    $dbPassword = $dbopts["pass"];
+    $dbName = ltrim($dbopts["path"],'/');
+    
+    try
+    {
+        // Create the PDO connection
+        $_SESSION['db'] = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+        // this line makes PDO give us an exception when there are problems, and can be very helpful in debugging!
+        $_SESSION['db']->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+    }
+    catch (PDOException $ex)
+    {
+        // If this were in production, you would not want to echo
+        // the details of the exception.
+        echo "Error connecting to DB. Details: $ex";
+        die();
+    }
 
     $_SESSION['username'] = $_POST['uname'];
     $_SESSION['password'] = $_POST['pass'];
