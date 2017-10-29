6204f85b1dc02c9b43262bc303b2e00d42c5a6dc
SimpleTrousers <simpletrousers2@gmail.com>
2017-10-28 21:31:25 -0600

    Better style for login and register page.

--- web/Prove06/login.php
+++ web/Prove06/login.php
@@ -9,7 +9,7 @@ session_start();
     <link rel="stylesheet" type="text/css" href="appointment.css">
 </head>
 <body>
-    <h1>Please Log In!</h1>
+    <h1>Please Login or Register an Account</h1>
 
     <form action="redirect.php" method="post">
         Username:
@@ -17,9 +17,10 @@ session_start();
         Password:
         <input type="password" name="pass"><br>
         <input type="hidden" name="pageFrom" value="loginPage">
-        <input type="submit" value="Login">
+        <input type="submit" class="submit" value="Login">
     </form>
-
-    <a href="https://cryptic-refuge-67781.herokuapp.com/Prove06/register.php">Or Please Create an Account!</a>
+    <form action="register.php" method="post">
+        <input type="submit" class="submit" value="Register">
+    </form>
 </body>
 </html>
---
+++ web/Prove06/register.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login Page</title>
+    <link rel="stylesheet" type="text/css" href="appointment.css">
+</head>
+<body>
+    <form action="login.php" method="post">
+        <input type="submit" class="submit" value="Login">
+    </form>
+</body>
+</html>
