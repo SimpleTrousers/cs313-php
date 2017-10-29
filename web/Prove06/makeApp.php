484cb07d029a62c1ca7900eecc5ec4af51e8e81c
SimpleTrousers <simpletrousers2@gmail.com>
2017-10-29 01:24:26 -0600

    Small change.

--- web/Prove06/makeApp.php
+++ web/Prove06/makeApp.php
@@ -7,12 +7,13 @@
 
     require("connectDB.php");
     $db = get_db();
-    $query = 'INSERT INTO appointment(military_hour, day, month, year, doctor_id, patient_id) VALUES(:mH, :day, :month, :year, 1, :id)';
+    $query = 'INSERT INTO appointment(military_hour, day, month, year, doctor_id, patient_id) VALUES(:mH, :day, :month, :year, :did, :id)';
     $stmnt = $db->prepare($query);
     $stmnt->bindValue(':mH', $mH);
     $stmnt->bindValue(':day', $day);
     $stmnt->bindValue(':month', $month);
     $stmnt->bindValue(':year', $year);
+    $stmnt->bindValue(':did', "1");
     $stmnt->bindValue(':id', $id);
     $stmnt->execute();
 
