d32f9d5b816b0d8fd334ac8075d462c3e95f59c8
SimpleTrousers <simpletrousers2@gmail.com>
2017-10-28 20:53:11 -0600

    Forgot to update link.

--- web/Prove06/redirect.php
+++ web/Prove06/redirect.php
@@ -5,6 +5,6 @@ $_SESSION['username'] = $_POST['uname'];
 $_SESSION['password'] = $_POST['pass'];
 
 if ($_POST['pageFrom'] == 'loginPage') {
-    header("Location: https://cryptic-refuge-67781.herokuapp.com/Prove05/appointment.php");
+    header("Location: https://cryptic-refuge-67781.herokuapp.com/Prove06/appointment.php");
 }
 ?>
